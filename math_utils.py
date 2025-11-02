c97a37be959b8936ce3311123f9466f46792c3c0
zivzolti-lab <zivzolti@gmail.com>
2025-11-02 21:40:15 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,7 +1,11 @@
-def find_max_number(num1, num2, num3):
-      if num1 > num2 and num1 > num3:
-        return num1
-      elif num2 > num3: 
-        return num2 
-      else:
-        return num3 
+def find_mean(num1, num2, num3):
+    pass  # Replace 'pass' with code
+    mean = (num1 + num2 + num3) / 3
+    return mean
+
+
+def find_mean_std(num1, num2, num3):
+    mean = find_mean(num1, num2, num3)
+    pass  # Replace 'pass' with code
+    std = ((((num1 - mean) ** 2) + ((num2 - mean) ** 2) + ((num3 - mean) ** 2)) / 3 ** 0.5)
+    return mean, std
